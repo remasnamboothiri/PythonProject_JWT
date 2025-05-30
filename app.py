73aea1fda8c62b9aebfcb6e8a696e5cc7c445c02
Rema S Nampoothiry <ramasnampoothiry@gmail.com>
2025-05-30 10:32:53 +0530

    requirement.txt and modified app.py

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, request, jsonify, render_template, redirect, url_for, session
 import jwt
 import datetime
@@ -113,4 +115,6 @@ def api_add(current_user):
     })
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port, debug=False)
+
